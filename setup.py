34c9d87d0e5fe6073b7bd28b09d269065aca4450
Christian Theune <ct@flyingcircus.io>
2011-02-08 10:14:08 +0100

    Include a reasonable long description.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-# Copyright (c) 2008 gocept gmbh & co. kg
+# Copyright (c) 2008-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id: setup.py 12305 2008-07-04 14:29:34Z zagy $
 
 from setuptools import setup, find_packages
+import os.path
 
+def read(*names):
+    return open(
+            os.path.join(os.path.dirname(__file__), *names)).read()
 
 setup(
     name='gocept.linkchecker',
@@ -11,6 +14,10 @@ setup(
     author='gocept gmbh & co. kg',
     author_email='mail@gocept.com',
     description='Check links in your Plone site using a link monitoring server.',
+    long_description=(
+        read('src', 'gocept', 'linkchecker', 'doc', 'README.txt') +
+        '\n\n' +
+        read('CHANGES.txt')),
     packages=find_packages('src'),
     package_dir={'':'src'},
     include_package_data=True,
