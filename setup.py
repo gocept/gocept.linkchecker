68818c7d60003c0764cf8dc25c773e40cb700fb3
Christian Theune <ct@flyingcircus.io>
2012-09-21 12:00:26 +0200

    Back to development: 3.0a9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*names):
 
 setup(
     name='gocept.linkchecker',
-    version='3.0a8',
+    version='3.0a9.dev0',
     author='gocept gmbh & co. kg',
     author_email='mail@gocept.com',
     description='Check links in your Plone site using a link monitoring server.',
